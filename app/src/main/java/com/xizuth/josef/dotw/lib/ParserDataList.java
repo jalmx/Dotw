2f6cc5fc4347ee04dbdb3271f146da830f6970a4
Josef Leyva <jalm_x@hotmail.com>
2017-10-22 18:51:35 -0500

    lib 100%

---
+++ app/src/main/java/com/xizuth/josef/dotw/lib/ParserDataList.java
@@ -0,0 +1,26 @@
+package com.xizuth.josef.dotw.lib;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * Created by josef on 10/22/17.
+ */
+
+public final class ParserDataList {
+
+    private ParserDataList() {
+    }
+
+    public static ArrayList<Double> listData(String dataText){
+        ArrayList<Double> data = new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(dataText,",");
+
+        while (tokenizer.hasMoreTokens()){
+            data.add(Double.parseDouble(tokenizer.nextToken()));
+        }
+
+        return data;
+    }
+}
